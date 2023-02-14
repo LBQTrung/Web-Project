6ced2eda85d228ec781a54c8f0f5069cd96278d3
Lê Bá Quốc Trung <91483513+LBQTrung@users.noreply.github.com>
2023-02-14 07:44:03 +0700

    Update play, pause, timeline of song

--- Music Player/main.js
+++ Music Player/main.js
@@ -1,7 +1,16 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-let app = {
+const player = $('.player')
+const cd = $(".cd")
+const heading = $('header h2')
+const cdThumb = $('.cd-thumb')
+const audio = $('#audio')
+const playBtn = $('.btn.btn-toggle-play')
+const progress = $('#progress')
+console.log(progress)
+
+let app = { 
     songs :  [
         {
             name: "Dẫu Có Lỗi Lầm",
@@ -51,6 +60,17 @@ let app = {
             image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
         }
     ],
+    currentIndex: 0,
+    isPlaying: false,
+
+    defineProperties: function() {
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
+
     render: function() {
         const htmls = this.songs.map((song) => {
             return `
@@ -70,8 +90,8 @@ let app = {
     },
 
     handleEvents: function() {
+        const _this = this
         document.onscroll = function() {
-            const cd = $(".cd")
             const cdWidth = cd.offsetWidth
             document.onscroll = function() {
                 const scrollTop = window.scrollY || document.documentElement.scrollTop
@@ -80,11 +100,46 @@ let app = {
                 cd.style.opacity = newCDWidth > 0 ? newCDWidth/cdWidth : 0
             }
         }
+
+        // Handle when click play button
+        playBtn.onclick = function() {
+            if (_this.isPlaying){
+                audio.pause()
+            } else {
+                audio.play()
+            }
+        }
+        // Handle play event
+        audio.onplay = function() {
+            _this.isPlaying = true
+            player.classList.add('playing')
+        }
+        // Handle pause event
+        audio.onpause = function() {
+            _this.isPlaying = false
+            player.classList.remove('playing')
+        }
+        // Handle timeline of song
+        audio.ontimeupdate = function() {
+            if (audio.duration) {
+                const progressPercent = Math.floor(audio.currentTime / audio.duration * 100)
+                progress.value = progressPercent 
+            }
+        }
+    },
+
+    loadCurrentSong: function() {
+        heading.textContent = this.currentSong.name
+        cdThumb.style.backgroundImage = `url(${this.currentSong.image})`
+        audio.src = this.currentSong.path
     },
 
     start: function() {
+        this.defineProperties()
         this.handleEvents()
+        this.loadCurrentSong()
         this.render()
     }
 }
 app.start()
+console.log(app.currentSong)
