2d265e5a606826b039f2be93f393cd379f79259b
Lê Bá Quốc Trung <91483513+LBQTrung@users.noreply.github.com>
2023-02-23 12:26:41 +0700

    Add files via upload

--- JavascriptBasicProject/VirtualPiano/main.js
+++ JavascriptBasicProject/VirtualPiano/main.js
@@ -1,6 +1,11 @@
 const pianoKeys = document.querySelectorAll('.key')
+const volumeSlider = document.querySelector('.volume-slider input')
+const checkBox = document.querySelector('.keys-checkbox input')
+const pianoKeyBoard = document.querySelectorAll('.piano-keys span')
+const validKeys = []
+console.log(pianoKeyBoard)
 
-let audio = new Audio('./tunes/a.wav') // default source
+let audio = new Audio() // default source
 
 const playTune = function(dataKey) {
     audio.src = `./tunes/${dataKey}.wav`
@@ -8,6 +13,9 @@ const playTune = function(dataKey) {
 }
 
 pianoKeys.forEach(function(key) {
+    // Save valid key
+    validKeys.push(key.dataset.key)
+    // Handle click event
     key.onclick = function() {
         playTune(key.dataset.key)
         key.classList.add('active')
@@ -18,10 +26,31 @@ pianoKeys.forEach(function(key) {
 })
 
 document.onkeydown = function(e){
-    playTune(e.key)
-    pianoKeys.forEach(function(key) {
-        if (key.dataset.key == e.key) {
-            key.click()
-        }
-    }) 
+    if (validKeys.includes(e.key)) {
+        playTune(e.key)
+        pianoKeys.forEach(function(key) {
+            if (key.dataset.key == e.key) {
+                key.click()
+            }
+        })
+    }   
 }
+
+volumeSlider.onchange = function(e) {
+    audio.volume = e.target.value
+}
+
+handleCheckedDisplay = function() {
+    if (checkBox.checked == true) {
+        pianoKeyBoard.forEach(function(key, index) {
+            key.innerText = pianoKeys[index].dataset.key
+        })
+        
+    } else {
+        pianoKeyBoard.forEach(function(key) {
+            key.innerText = ''
+        })
+    }
+}
+
+checkBox.onclick = handleCheckedDisplay
